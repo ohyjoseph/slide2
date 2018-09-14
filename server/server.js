3e30552b74b227f072073c6f4af84d3f4885d9ef
Joseph Oh <ohyjoseph@gmail.com>
2018-09-14 16:51:55 -0500

    Added test server route

--- server/server.js
+++ server/server.js
@@ -4,8 +4,12 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get('/helloWorld', function (req, res) {
+  console.log('helloWorld')
 });
 
 app.listen(9000);
